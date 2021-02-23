c61de5e39a7821456ee8e495b3ede6b0d42331a9
Hyun-gin-Cho <79429757+Hyun-gin-Cho@users.noreply.github.com>
2021-02-23 15:01:51 +0900

    dto에 getter&setter 생성

--- WYW/src/com/mvc/dto/UserDataDto.java
+++ WYW/src/com/mvc/dto/UserDataDto.java
@@ -27,6 +27,204 @@ public class UserDataDto {
 	private Date regdate;		//글 등록 날자
 	
 	public UserDataDto() {}
+
+	public UserDataDto(int userno, String userid, String userpw, String username, String useraddr, String userphone,
+			String useremail, String userenabled, String userrole, int userfollower, int userfollowing, int boardno,
+			int groupno, int groupsq, String title, String content, String contentrole, String userimgname,
+			String userimg, int userlike, Date regdate) {
+		super();
+		this.userno = userno;
+		this.userid = userid;
+		this.userpw = userpw;
+		this.username = username;
+		this.useraddr = useraddr;
+		this.userphone = userphone;
+		this.useremail = useremail;
+		this.userenabled = userenabled;
+		this.userrole = userrole;
+		this.userfollower = userfollower;
+		this.userfollowing = userfollowing;
+		this.boardno = boardno;
+		this.groupno = groupno;
+		this.groupsq = groupsq;
+		this.title = title;
+		this.content = content;
+		this.contentrole = contentrole;
+		this.userimgname = userimgname;
+		this.userimg = userimg;
+		this.userlike = userlike;
+		this.regdate = regdate;
+	}
+
+	public int getUserno() {
+		return userno;
+	}
+
+	public void setUserno(int userno) {
+		this.userno = userno;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getUserpw() {
+		return userpw;
+	}
+
+	public void setUserpw(String userpw) {
+		this.userpw = userpw;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUseraddr() {
+		return useraddr;
+	}
+
+	public void setUseraddr(String useraddr) {
+		this.useraddr = useraddr;
+	}
+
+	public String getUserphone() {
+		return userphone;
+	}
+
+	public void setUserphone(String userphone) {
+		this.userphone = userphone;
+	}
+
+	public String getUseremail() {
+		return useremail;
+	}
+
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+
+	public String getUserenabled() {
+		return userenabled;
+	}
+
+	public void setUserenabled(String userenabled) {
+		this.userenabled = userenabled;
+	}
+
+	public String getUserrole() {
+		return userrole;
+	}
+
+	public void setUserrole(String userrole) {
+		this.userrole = userrole;
+	}
+
+	public int getUserfollower() {
+		return userfollower;
+	}
+
+	public void setUserfollower(int userfollower) {
+		this.userfollower = userfollower;
+	}
+
+	public int getUserfollowing() {
+		return userfollowing;
+	}
+
+	public void setUserfollowing(int userfollowing) {
+		this.userfollowing = userfollowing;
+	}
+
+	public int getBoardno() {
+		return boardno;
+	}
+
+	public void setBoardno(int boardno) {
+		this.boardno = boardno;
+	}
+
+	public int getGroupno() {
+		return groupno;
+	}
+
+	public void setGroupno(int groupno) {
+		this.groupno = groupno;
+	}
+
+	public int getGroupsq() {
+		return groupsq;
+	}
+
+	public void setGroupsq(int groupsq) {
+		this.groupsq = groupsq;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getContentrole() {
+		return contentrole;
+	}
+
+	public void setContentrole(String contentrole) {
+		this.contentrole = contentrole;
+	}
+
+	public String getUserimgname() {
+		return userimgname;
+	}
+
+	public void setUserimgname(String userimgname) {
+		this.userimgname = userimgname;
+	}
+
+	public String getUserimg() {
+		return userimg;
+	}
+
+	public void setUserimg(String userimg) {
+		this.userimg = userimg;
+	}
+
+	public int getUserlike() {
+		return userlike;
+	}
+
+	public void setUserlike(int userlike) {
+		this.userlike = userlike;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
 	
 	
 	
