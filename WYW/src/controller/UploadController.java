2ce77951c1e9ff5f8346ef99d20cde1536f476fd
Hyun-gin-Cho <79429757+Hyun-gin-Cho@users.noreply.github.com>
2021-03-07 16:11:38 +0900

    업로드 서비스 업데이트 테스트

--- WYW/src/controller/UploadController.java
+++ WYW/src/controller/UploadController.java
@@ -1,11 +1,19 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.mvc.dao.UserDataDao;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 @WebServlet("/UploadController")
 public class UploadController extends HttpServlet {
@@ -18,6 +26,20 @@ public class UploadController extends HttpServlet {
 		String fileName = request.getParameter("file");
         System.out.println(fileName);
         
+        ServletContext context = getServletContext(); //어플리케이션에 대한 정보를 ServletContext 객체가 갖게 됨. 
+        String saveDir = context.getRealPath("Upload"); //절대경로를 가져옴
+        System.out.println("절대경로 >> " + saveDir);
+        
+        int maxSize = 500*1024*1024; // 500MB
+        String encoding = "euc-kr";
+        
+        boolean isMulti = ServletFileUpload.isMultipartContent(request);
+        if(isMulti) {
+        	 MultipartRequest multi = new MultipartRequest(request, saveDir, maxSize, encoding, new DefaultFileRenamePolicy());
+        	 UserDataDao dao = new UserDataDao();
+        	 
+        	 //String 
+        }
         
 	}
 
